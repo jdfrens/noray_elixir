b292a56186a4d43c1c51a4c09d4b173532a757e1
Jeremy D. Frens <jdfrens@gmail.com>
2020-08-25 14:08:38 -0500

    [issue-1] clean up initial project

--- lib/noray.ex
+++ lib/noray.ex
@@ -2,17 +2,4 @@ defmodule Noray do
   @moduledoc """
   Documentation for Noray.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Noray.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/noray_test.exs
+++ test/noray_test.exs
@@ -1,8 +1,4 @@
 defmodule NorayTest do
   use ExUnit.Case
   doctest Noray
-
-  test "greets the world" do
-    assert Noray.hello() == :world
-  end
 end
