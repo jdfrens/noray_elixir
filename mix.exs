070711f43b1deaebf83c232bcf4d68ace8022d44
Jeremy D. Frens <jdfrens@gmail.com>
2020-08-25 14:10:40 -0500

    [issue-1] initial all_tests alias and GitHub action for CI test

--- mix.exs
+++ mix.exs
@@ -3,11 +3,25 @@ defmodule Noray.MixProject do
 
   def project do
     [
+      aliases: aliases(),
       app: :noray,
       deps: deps(),
       elixir: "~> 1.10",
+      preferred_cli_env: [
+        all_tests: :test,
+      ],
       start_permanent: Mix.env() == :prod,
       version: "0.1.0"
+    ]
+  end
+
+  defp aliases do
+    [
+      all_tests: [
+        "deps.get",
+        "deps.compile",
+        "compile --force --return-errors",
+      ]
     ]
   end
 
